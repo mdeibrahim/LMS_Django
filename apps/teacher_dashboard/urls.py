f3aff34c08585740eace4b72df519694c09d481f
mdeibrahim <mmeibrahim505@gmail.com>
2026-06-28 10:09:00 +0600

    added profilr update page

--- apps/teacher_dashboard/urls.py
+++ apps/teacher_dashboard/urls.py
@@ -3,14 +3,18 @@ from .views import LoginView, RegisterView, TeacherProfileView, CourseListView, 
 
 
 urlpatterns = [
+    # Auth URLs 
     path('auth/register/', RegisterView.as_view(), name='teacher_register'),
     path('auth/login/', LoginView.as_view(), name='teacher_login'),
     path('profile/', TeacherProfileView.as_view(), name='teacher_profile'),
     path('update-profile/', TeacherProfileView.as_view(), name='teacher_update_profile'),
 
+    # Course and Subcategory URLs
     path('category-subcategory-list/', CategorySubcategoryListView.as_view(), name='teacher_category_subcategory_list'),
     path('create-subcategory/', SubcategoryCreateView.as_view(), name='teacher_create_subcategory'),
+    
 
+    # Course URLs
     path('create-course/', CourseListView.as_view(), name='teacher_create_course'),
     path('course-list/', CourseListView.as_view(), name='teacher_course_list'),
 
